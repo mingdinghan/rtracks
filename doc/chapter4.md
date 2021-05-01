8d3feef8318346759e6f1ce99a76f12cc519788d
Mingding Han <mingdinghan@gmail.com>
2021-05-02 00:06:32 +0800

    test: Update tests to override routing behavior

--- doc/chapter4.md
+++ doc/chapter4.md
@@ -1,3 +1,5 @@
 ## Chapter 4 Notes
 
 - `Erubis` is the implementation of `Erb` that Rails uses
+- build a basic set of views with Erb support, turning Erb templates into text
+  - extract controller class name to render views in corresponding directory
--- test/application_test.rb
+++ test/application_test.rb
@@ -1,6 +1,15 @@
 require_relative "test_helper"
 
+class TestController < Rtracks::Controller
+  def index
+    "Hello!"
+  end
+end
+
 class TestApp < Rtracks::Application
+  def get_controller_and_action(env)
+    [TestController, "index"]
+  end
 end
 
 class RtracksAppTest < Minitest::Test
@@ -11,10 +20,10 @@ class RtracksAppTest < Minitest::Test
   end
 
   def test_request
-    get "/"
+    get "/example/anything"
 
     assert last_response.ok?
     body = last_response.body
-    assert body["This is an empty homepage"]
+    assert body["Hello"]
   end
 end
